ae6447d8619cd7580c3c3923432f11c107fcee8e
Mihran Galstyan <msho.mher@gmail.com>
2021-11-08 19:18:20 +0400

    Add project files.

---
+++ StaticClassesAndExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassesAndExtensions.Extensions
+{
+    static class SecondStaticClass
+    {
+        /// <summary>
+        /// Creating an extension for class String
+        /// </summary>
+        /// <param name="text">Given text</param>
+        /// <param name="textForPrint">Given text for realisation</param>
+        public static void PrintStringExtension(this String text, String textForPrint)
+        {
+            Console.WriteLine(textForPrint);
+        }
+    }
+}
---
+++ StaticClassesAndExtensions/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using StaticClassesAndExtensions.Extensions;
+
+namespace StaticClassesAndExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // not working creation of static class object
+            // StaticClass newStatic = new StaticClass();
+
+            // working call of static method of static slass;
+            StaticClass.PrintFunc();
+            StaticClass.ModFunc();
+
+            // working value assignment of static variable of static class
+            StaticClass.firstVariable = 0;
+
+            // using an extension for "String"
+            String longText = "default text";
+            longText.PrintStringExtension("Extension text");
+
+        }
+
+        public static class StaticClass
+        {
+            /// <summary>
+            /// static variable
+            /// </summary>
+            public static int firstVariable;
+
+            /// <summary>
+            /// Static void method
+            /// </summary>
+            public static void PrintFunc()
+            {
+                Console.WriteLine($"Here is value of static variable: {firstVariable}");
+            }
+
+            /// <summary>
+            /// Static method
+            /// </summary>
+            /// <returns>Integer value</returns>
+            public static int ModFunc()
+            {
+                if (firstVariable == 0)
+                {
+                    return firstVariable;
+                }
+                return default(int);
+            }
+
+            /// <summary>
+            /// Static constructor
+            /// </summary>
+            static StaticClass()
+            {
+                firstVariable = 15;
+            }
+        }
+    }
+
+}
